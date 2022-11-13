adffaa21eac748b3429a77eacc3d05db3a6e8cb3
Horace <horace@abenga.com>
2022-11-13 18:08:56 +0300

    Rename example

---
+++ 0001_sum_natural_numbers/src/main.rs
@@ -0,0 +1,20 @@
+
+use std::io;
+
+fn main() {
+    println!("Please submit the limit");
+    let mut limit = String::new();
+
+    io::stdin().read_line(&mut limit).expect("Failed to read limit");
+
+    let limit: i32 = limit.trim().parse().expect("Expected an integer!");
+
+    let mut total = 0;
+    for i in 1..(limit+1) {
+        println!("{}", i);
+        if i % 3 == 0 || i % 5 == 0 {
+            total += i;
+        }
+    }
+    println!("total = {}", total);
+}
